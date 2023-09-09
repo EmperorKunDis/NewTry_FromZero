3dba0e32730141629eff28a4b589e0ee4d2bc83a
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-09 09:52:12 +0200

    For Each Loop
    In diesem Program werde ich die sogenannte "for-each"-Schleife, auch als erweiterte "for"-Schleife bekannt, erklären.
    Dies ist eine Durchlauftechnik, um alle Elemente in einem Array oder einer anderen Sammlung zu durchlaufen.
    Einer der Vorteile besteht darin, dass eine "for-each"-Schleife weniger Schritte benötigt und lesbarer ist als eine
    herkömmliche "for"-Schleife. Es ist jedoch wichtig zu beachten, dass sie weniger flexibel ist als eine herkömmliche
    "for"-Schleife.

--- TwoD_Array/src/For_Each_Loop.java
+++ TwoD_Array/src/For_Each_Loop.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class For_Each_Loop {
+
+    public static void main(String[] args) {
+
+        String[] pokemons = {"WaterType","FireType","LightningType","PsychicType"};
+
+        ArrayList<String> animals = new ArrayList<String>();
+
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rat");
+        animals.add("bird");
+
+        for(String i : pokemons) {
+            System.out.println(i);
+        }
+        for(String i : animals) {
+            System.out.println(i);
+
+        }
+    }
+
 }
