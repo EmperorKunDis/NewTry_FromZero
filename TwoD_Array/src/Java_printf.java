e6efdc7c73c5b0ba9d1bc85734d02f7f8a387166
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-10 07:13:11 +0200

    PrintF methos with two arguments

--- TwoD_Array/src/Java_printf.java
+++ TwoD_Array/src/Java_printf.java
@@ -30,7 +30,7 @@ public class Java_printf {
             y = "Joxer";
             MyStr ="Loser.UAre";
             MyInt = 85;
-            MyDub = 1313;
+            MyDub = 131545641;
         }
 
         char MyChar = '@';
@@ -41,7 +41,7 @@ public class Java_printf {
         System.out.printf("And your email is :  %s"+MyChar+MyStr,y);                // %c for Character=char
         System.out.printf("\n"+"You are %d.",MyInt);                                // %d for Integer=int
         System.out.printf("You was born at %f year, like true "+y+".",MyDub);       // %f for Double=double
-
+        System.out.printf("It was %,f years, back.",MyDub);
         // [width/breite/sirka] - Minimum characters to be written as output.
 
     }
